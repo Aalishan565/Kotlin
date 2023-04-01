ae91574ff0aa0d3307e32e8fe103af627e6e120a
aalishanansari <aalishan@payo.in>
2023-04-01 16:40:37 +0530

    -fibonacci program

---
+++ src/_1_general/_7_Fibonacci.kt
@@ -0,0 +1,27 @@
+package _1_general
+
+fun main() {
+    fibonacciResultOfGivenNumber(7)
+}
+
+fun fibonacciUsingLoop(number: Int) {
+    var firstNumber = 0
+    var secondNumber = 1
+    var nextNumber: Int
+    println(firstNumber)
+    println(secondNumber)
+
+    for (i in 3..number) {
+        nextNumber = firstNumber + secondNumber
+        println(nextNumber)
+        firstNumber = secondNumber
+        secondNumber = nextNumber
+    }
+}
+
+fun fibonacciResultOfGivenNumber(number: Int): Int {
+    if (number <= 1) {
+        return 1
+    }
+    return fibonacciResultOfGivenNumber(number - 1) * fibonacciResultOfGivenNumber(number - 2)
+}
